1cc8e5bddbfc8fd1fb205b0028c1acbd62331e68
JauntyJJS <jauntyjjs@hotmail.com>
2020-12-18 16:34:47 +0800

    Fixed bugs in documentation

--- R/dilplots-ggplot.R
+++ R/dilplots-ggplot.R
@@ -156,6 +156,63 @@ dilution_summary_num_table <- function(dilution_summary_grp) {
 #' name is a dilution batch group given in `dil_batch_var`
 #' @return Output ggplot dilution plot data of one dilution batch per transition
 #' @rdname dilution_plot_ggplot
+#' @examples
+#' # Data Creation
+#' dilution_percent <- c(10, 20, 25, 40, 50, 60,
+#'                       75, 80, 100, 125, 150)
+#' sample_name <- c("Sample_010a", "Sample_020a",
+#'                  "Sample_025a", "Sample_040a", "Sample_050a",
+#'                  "Sample_060a", "Sample_075a", "Sample_080a",
+#'                  "Sample_100a", "Sample_125a", "Sample_150a")
+#' dilution_batch <- c("B1", "B1", "B1", "B1", "B1",
+#'                     "B1", "B1", "B1", "B1", "B1", "B1")
+#' transition_name <- c("Lipid1", "Lipid1", "Lipid1", "Lipid1",
+#'                      "Lipid1", "Lipid1", "Lipid1", "Lipid1",
+#'                      "Lipid1", "Lipid1", "Lipid1")
+#' lipid1_area_saturated <- c(5748124, 16616414, 21702718, 36191617,
+#'                            49324541, 55618266, 66947588, 74964771,
+#'                            75438063, 91770737, 94692060)
+#'
+#' dilution_data <- tibble::tibble(Sample_Name = sample_name,
+#'                                 Dilution_Batch_Name = dilution_batch,
+#'                                 Dilution_Percent = dilution_percent,
+#'                                 Transition_Name = transition_name,
+#'                                 Area = lipid1_area_saturated,
+#' )
+#'
+#' grouping_variable <- c("Transition_Name","Dilution_Batch_Name")
+#'
+#' # Get the dilution batch name from dilution_table
+#' dilution_batch_name <- dilution_batch %>%
+#'   unique() %>%
+#'   as.character()
+#'
+#' dil_batch_col = c("#377eb8")
+#'
+#' # Create palette for each dilution batch for plotting
+#' pal <- dil_batch_col %>%
+#'   stats::setNames(dilution_batch_name)
+#'
+#'
+#' # Create dilution statistical summary
+#' dilution_summary_grp <- dilution_data %>%
+#'   summarise_dilution_table(grouping_variable = grouping_variable,
+#'                            conc_var = "Dilution_Percent",
+#'                            signal_var = "Area") %>%
+#'   evaluate_linearity(grouping_variable = grouping_variable) %>%
+#'   dplyr::select(-c(dplyr::all_of(grouping_variable)))
+#'
+#'
+#' # Create the ggplot
+#' p <- dilution_plot_ggplot(dilution_data,
+#'                           dilution_summary_grp = dilution_summary_grp,
+#'                           title = "Lipid_Saturated",
+#'                           dil_batch_var = "Dilution_Batch_Name",
+#'                           conc_var = "Dilution_Percent",
+#'                           conc_var_units = "%",
+#'                           conc_var_interval = 50,
+#'                           signal_var = "Area",
+#'                           pal = pal)
 #' @export
 dilution_plot_ggplot <- function(dilution_data,
                                  dilution_summary_grp,
@@ -414,6 +471,75 @@ dilution_plot_ggplot <- function(dilution_data,
 #' in each row. This column is used to create the plot figure in the
 #' pdf report.
 #' @rdname create_ggplot_table
+#' @examples
+#' # Data Creation
+#' dilution_percent <- c(10, 20, 25, 40, 50, 60,
+#'                       75, 80, 100, 125, 150,
+#'                       10, 25, 40, 50, 60,
+#'                       75, 80, 100, 125, 150)
+#' dilution_batch <- c("B1", "B1", "B1", "B1", "B1",
+#'                     "B1", "B1", "B1", "B1", "B1", "B1",
+#'                     "B2", "B2", "B2", "B2", "B2",
+#'                     "B2", "B2", "B2", "B2", "B2")
+#' sample_name <- c("Sample_010a", "Sample_020a",
+#'                  "Sample_025a", "Sample_040a", "Sample_050a",
+#'                  "Sample_060a", "Sample_075a", "Sample_080a",
+#'                  "Sample_100a", "Sample_125a", "Sample_150a",
+#'                  "Sample_010b", "Sample_025b",
+#'                  "Sample_040b", "Sample_050b", "Sample_060b",
+#'                  "Sample_075b", "Sample_080b", "Sample_100b",
+#'                  "Sample_125b", "Sample_150b")
+#' lipid1_area_saturated <- c(5748124, 16616414, 21702718, 36191617,
+#'                            49324541, 55618266, 66947588, 74964771,
+#'                            75438063, 91770737, 94692060,
+#'                            5192648, 16594991, 32507833, 46499896,
+#'                            55388856, 62505210, 62778078, 72158161,
+#'                            78044338, 86158414)
+#' lipid2_area_linear <- c(31538, 53709, 69990, 101977, 146436, 180960,
+#'                         232881, 283780, 298289, 344519, 430432,
+#'                         25463, 63387, 90624, 131274, 138069,
+#'                         205353, 202407, 260205, 292257, 367924)
+#' lipid3_area_lod <- c(544, 397, 829, 1437, 1808, 2231,
+#'                      3343, 2915, 5268, 8031, 11045,
+#'                      500, 903, 1267, 2031, 2100,
+#'                      3563, 4500, 5300, 8500, 10430)
+#' lipid4_area_nonlinear <- c(380519, 485372, 478770, 474467, 531640, 576301,
+#'                            501068, 550201, 515110, 499543, 474745,
+#'                            197417, 322846, 478398, 423174, 418577,
+#'                            426089, 413292, 450190, 415309, 457618)
+#'
+#' dilution_annot <- tibble::tibble(Sample_Name = sample_name,
+#'                                  Dilution_Batch = dilution_batch,
+#'                                  Dilution_Percent = dilution_percent)
+#' lipid_data <- tibble::tibble(Sample_Name = sample_name,
+#'                              Lipid1 = lipid1_area_saturated,
+#'                              Lipid2 = lipid2_area_linear,
+#'                              Lipid3 = lipid3_area_lod,
+#'                              Lipid4 = lipid4_area_nonlinear)
+#'
+#'
+#' # Create dilution table
+#' dilution_table <- create_dilution_table(dilution_annot, lipid_data,
+#'                                         common_column = "Sample_Name",
+#'                                         signal_var = "Area",
+#'                                         column_group = "Transition_Name"
+#' )
+#'
+#' # Create dilution table and dilution statistical summary
+#' dilution_summary <- dilution_table %>%
+#'   summarise_dilution_table(grouping_variable = c("Transition_Name",
+#'                                                  "Dilution_Batch"),
+#'                            conc_var = "Dilution_Percent",
+#'                            signal_var = "Area") %>%
+#'   dplyr::arrange(.data$Transition_Name) %>%
+#'   evaluate_linearity(grouping_variable = c("Transition_Name",
+#'                                            "Dilution_Batch"))
+#'
+#'
+#' # Create a ggplot table
+#' ggplot_table <- create_ggplot_table(dilution_table,
+#'                                     dilution_summary = dilution_summary)
+#'
 #' @export
 create_ggplot_table <- function(dilution_table, dilution_summary = NULL,
                                 grouping_variable = c("Transition_Name",
