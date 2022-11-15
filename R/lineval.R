21d5c879cd6bf0167649bb90c635f4d082c792e5
JauntyJJS <jauntyjjs@hotmail.com>
2022-11-15 13:16:11 +0800

    Change the input of the dplyr::relocate to only have dplyr::any_of(var) instead of .data[[var]]

--- R/lineval.R
+++ R/lineval.R
@@ -104,10 +104,16 @@ evaluate_linearity <- function(dilution_summary,
 
     # Rearrange the column based on the first evaluation
     dilution_summary <- dilution_summary %>%
+      # dplyr::relocate(
+      #   dplyr::any_of(grouping_variable),
+      #   .data[["wf1_group"]], .data[[corrcoef_column]],
+      #   .data[[pra_column]]
+      # )
       dplyr::relocate(
-        dplyr::any_of(grouping_variable),
-        .data[["wf1_group"]], .data[[corrcoef_column]],
-        .data[[pra_column]]
+        dplyr::any_of(
+          c(grouping_variable,"wf1_group",
+            corrcoef_column, pra_column)
+        )
       )
 
     return(dilution_summary)
@@ -134,12 +140,20 @@ evaluate_linearity <- function(dilution_summary,
 
   # Rearrange the column based on the first evaluation
   dilution_summary <- dilution_summary %>%
+    # dplyr::relocate(
+    #   dplyr::any_of(grouping_variable),
+    #   .data[["wf1_group"]], .data[["wf2_group"]],
+    #   .data[[corrcoef_column]], .data[[pra_column]],
+    #   .data[[mandel_p_val_column]], .data[[concavity_column]]
+    # )
     dplyr::relocate(
-      dplyr::any_of(grouping_variable),
-      .data[["wf1_group"]], .data[["wf2_group"]],
-      .data[[corrcoef_column]], .data[[pra_column]],
-      .data[[mandel_p_val_column]], .data[[concavity_column]]
+      dplyr::any_of(
+        c(grouping_variable,"wf1_group", "wf2_group",
+          corrcoef_column, pra_column,
+          mandel_p_val_column, concavity_column)
+      )
     )
+
 
   return(dilution_summary)
 }
